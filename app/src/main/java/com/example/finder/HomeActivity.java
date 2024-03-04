4510814a9904aedf1581f6fbcd221ea89e79ea1a
DESKTOP-G0COV8E\David <david.garcia.0410@gmail.com>
2024-03-04 20:54:09 +0100

    Se agrego mapa y se ignoro AndroidManifest.xml

--- app/src/main/java/com/example/finder/HomeActivity.java
+++ app/src/main/java/com/example/finder/HomeActivity.java
@@ -2,7 +2,10 @@ package com.example.finder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button btMap = findViewById(R.id.btMap);
+        btMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
